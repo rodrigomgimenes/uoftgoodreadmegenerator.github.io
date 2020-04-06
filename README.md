4f16e46a26c3f79a1577dd87b0fbea9b6f906143
Rodrigo Gimenes <rodrigomacedogimenes@gmail.com>
2020-04-06 09:57:26 -0400

    good readme project completed

---
+++ README.md
@@ -0,0 +1,77 @@
+# Project: uofresponsiveportfolio by Rodrigo M. Gimenes 
+# ====================================================================================================
+
+  ## File:
+
+  * ['portfolio-webpage'](https://rodrigomgimenes.github.io/uofresponsiveportfolio.github.io/)
+
+  ## Description:
+  Homework 02 - Responsive Portfolio using the Bootstrap CSS Framework
+
+
+  ## Instructions:
+
+  * This webpage is a coding assignment, which is based on concepts of HTML,CSS,Javascript,Jquery.
+
+  * The webpage consists in the following page(s): index.html, about.html
+
+  * Once the main webpage (index.html) loads completely, the user will be presented with the following elements:
+    1. //Describe your webpage here!
+
+  * How to use?
+    1. //Describe how to use your webpage here!
+
+  * Hope you enjoy it!!
+
+
+  ## Basic Informations: 
+
+  * This app runs in the browser and features dynamically updated HTML and CSS powered by Javascript and Jquery code. It also features a clean and polished user interface and it is responsive, adapting to multiple screen sizes.
+
+
+  ## Installation:
+
+  * The installation process will proceed as it follows:
+    1. //Describe the steps of your application install here!
+  or
+  * No installation required!
+
+
+  ## Usage: 
+
+  * The content of this project is purely Academic.
+
+
+  ## License: None
+
+
+  ## Contributing:
+  
+  * This webpage does not have collaborators.
+  
+  
+  ## Tests:
+
+  * In this project, the following testing procedures were performed:
+    1. //Describe the steps of your application tests here!
+  or
+  * No testing procedures were performed in this project.
+
+
+  ## Author:
+  * This app was design by ['Rodrigo M. Gimenes'](https://avatars1.githubusercontent.com/u/59060046?v=4) 
+  * Contact Me: 
+    1. Email: -
+    2. LinkedIn: ['My Profile'](https://www.linkedin.com/in/rodrigo-m-gimenes-b0a1a227/) 
+  
+  ## Credits:
+
+  * Images:
+        ['Google'](https://www.google.ca/)
+* Icons & Fonts:
+        ['Google'](https://www.google.ca/)
+        ['Google-Fonts'](https://fonts.google.com/)
+        ['Font-Awesome'](https://fontawesome.com/?from=io)
+* Other:
+  ['GitHub'](https://github.com/rodrigomgimenes)
+  
--- index.js
+++ index.js
@@ -1,25 +1,140 @@
-const fs = require("fs");
+const fs       = require("fs");
 const inquirer = require("inquirer");
+const path     = require('path');
 // Javascript Files:
-const api = require("./utils/api");
+const api              = require("./utils/api");
 const generateMarkdown = require("./utils/generateMarkdown");
 
-const questions = [
+let questions = [
   {
     type:    "input",
-    message: "Please, enter your GitHub username: ",
-    name:    "username"
+    name:    "username",
+    message: "Please, enter your GitHub username: "
   }
 ];
 
-function writeToFile(fileName, data) {
-  // const writeFileAsync = util.promisify(fs.writeFile);
+let requirements = [
+  {
+    type:    "input",
+    name:    "tablecontents",
+    message: "Describe the Contents of your project."
+  },
+  {
+    type:    "list",
+    name:    "dynamic",
+    message: "Does your project present features dynamically updated?",
+    choices: [
+      "Yes",
+      "No"
+    ]
+  },
+  {
+    type:    "input",
+    name:    "installation",
+    message: "Briefly describe the method of installation."
+  },
+  {
+    type:    "input",
+    name:    "pages",
+    message: 'Name each page of your project separated by comma (",").'
+  },
+  {
+    type:    "list",
+    name:    "usage",
+    message: "What is the purpose of your project?",
+    choices: [
+      "Academic",
+      "Commercial"
+    ]
+  },
+  {
+    type:    "input",
+    name:    "contributing",
+    message: 'If your project was collaborative, present the name(s) of your collaborator(s) separated by comma (","). Otherwise, press ENTER.'
+  },
+  {
+    type:    "input",
+    name:    "tests",
+    message: "If you developed tests outlining the expected functionality for implemented application code, briefly describe your tests. Otherwise, press ENTER."
+  },
+  {
+    type:    "checkbox",
+    name:    "languages",
+    message: "Which languages were used?",
+    choices: [
+      "HTML",
+      "CSS",
+      "Javascript",
+      "Jquery"
+    ]
+  },
+  {
+    type:    "checkbox",
+    name:    "fonts",
+    message: "If you make use of differrent fonts and or icons in your project, select one or more options below. Otherwise, select SKIP.",
+    choices: [
+      "Google",
+      "Google-Fonts",
+      "Font-Awesome",
+      "SKIP"
+    ]
+  },
+  {
+    type:    "checkbox",
+    name:    "images",
+    message: "If you make use of differrent images in your project, select one or more options below. Otherwise, select SKIP.",
+    choices: [
+      "Google",
+      "Pinterest",
+      "Flickr",
+      "SKIP"
+    ]
+  }
+];
+
+function writeToFile(data, ...fileName) {
+  for (let index = 0; index < fileName.length; index++) {
+    console.log(`Creating "${fileName[index]}" file...`)
+
+    fs.writeFileSync(path.join(process.cwd(), fileName[index]), data[index], function(err){
+      if (err) {
+        console.log(`ERROR: "${fileName[index]}" file was not create due to ${err}.`);
+      }
+    });
+    console.log(`"${fileName[index]}" file successfully created!`); 
+  }
 }
 
 function init() {
-  inquirer.prompt(questions[0]).then( (response) => {
-    api.getUser(response).then(({ res }) => {
-      console.log("Response.Data = ", res.data);
+  inquirer.prompt(questions[0]).then( (response_Q0) => {
+    api.getUser(response_Q0.username).then((resUser) => {
+      // console.log("Response.Data = ", resUser.data);
+
+      api.getRepository(response_Q0.username).then((resRepo) => {
+        const repoNames = resRepo.data.map((repository) => repository.name);
+        // console.log("Chosen One: ", resRepo.data);
+
+        if (repoNames.length > 0) {
+          // Put ALL repositories as options to the user
+          questions.push( 
+            {
+              type:    "list",
+              name:    "repolist",
+              message: 'Please, choose 1 (one) repository to create your new "Readme.md" file:',
+              choices: repoNames
+            }
+          );
+
+          inquirer.prompt(questions[questions.length - 1]).then( (response_Q1) => {
+            // console.log(`Data from Chosen One: ${resRepo.data[questions[questions.length - 1].choices.indexOf(response_Q1.repolist)].name}`);
+            inquirer.prompt(requirements).then( (responseRequirements) => {
+              writeToFile(generateMarkdown(resUser.data, resRepo.data[questions[questions.length - 1].choices.indexOf(response_Q1.repolist)], responseRequirements, repoNames), "README.md", `${resUser.data.login}.pdf`);
+            });
+          });
+        }
+        else
+          console.log(`WARNING: "${response_Q0.username}" does not have a repository.`);
+      });
     });
   });
 }
